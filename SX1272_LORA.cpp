f1df07241b2d93e5db1c205c9ff91b82734fd9f6
chisl <info@chisl.io>
2018-01-23 05:49:06 +0100

    update

--- SX1272_LORA.cpp
+++ SX1272_LORA.cpp
@@ -2,12 +2,17 @@
  * name:        SX1272
  * description: 860 MHz to 1020 MHz Low Power Long Range Transceiver featuring the LoRa (TM) long range modem
  * manuf:       Semtech
- * version:     0.1
- * url:         http://www.semtech.com/images/datasheet/sx1272.pdf
+ * version:     Version 0.1
+ * url:         https://www.semtech.com/uploads/documents/sx1272.pdf
  * date:        2016-08-01
  * author       https://chisl.io/
  * file:        SX1272_LORA.cpp
  */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
 
 #include "SX1272_LORA.hpp"
 
